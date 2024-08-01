5df7d251f50af6f971a49c1dec84ec1a4fc85473
Donopha Cletus <quadkast@developers.in>
2024-08-01 16:43:41 +0530

    bug fix

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,4 @@
-/**
- * Default CSS definition for typescript,
- * will be overridden with file-specific definitions by rollup
- */
+
 declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
